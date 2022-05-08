b0ec036e3ff8bd37fa8c2ac452c197b3c7708631
Adrien Pauchet <grandchefdotkong@gmail.com>
2022-05-08 20:57:47 +0900

    first commit

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,18 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <p>{{ `name: ${name}, age: ${age}` }}</p>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'HomeView',
-  components: {
-    HelloWorld
+  setup() {
+    let name = 'mario';
+    let age = 30;
+
+    return { name, age }
   }
 }
 </script>
